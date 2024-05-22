51ecdb675ac24bad562c3e6648fbea05201354c6
MMarinov97 <marinovantonov@gmail.com>
2024-05-23 00:46:44 +0200

    no message

--- Tetris/GameGrid.cs
+++ Tetris/GameGrid.cs
@@ -48,9 +48,25 @@ namespace Tetris
             return IsInside(r, c) && grid[r, c] == 0;
         }
         
+        /// <summary>
+        /// Checks if all cells in a row are full
+        /// </summary>
+        /// <param name="r">Row</param>
+        /// <returns></returns>
         public bool IsRowFull(int r)
         {
-            return Enumerable.Range(0, grid.GetLength(0))
+            return Enumerable.Range(0, grid.GetLength(1))
+                .All(c => grid[r, c] != 0);
+        }
+        
+        /// <summary>
+        /// Checks if all cells in a row are empty
+        /// </summary>
+        /// <param name="r">Row</param>
+        /// <returns></returns>
+        public bool IsRowEmpty(int r)
+        {
+            return Enumerable.Range(0, grid.GetLength(1))
                 .All(c => grid[r, c] == 0);
         }
     }
